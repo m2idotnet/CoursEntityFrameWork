b1b769aff4f8cf350495f7692d3aa6d2be014b6d
m2i <m2idotnet@gmail.com>
2019-06-07 11:43:05 +0200

    Linq to Entities

---
+++ CoursEntityFrameWork/CoursLinqToEntities/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursLinqToEntities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base(@"Data Source=(LocalDb)\DataBaseEntityFrameWork;Integrated Security=True")
+        {
+
+        }
+
+        public DbSet<Personne> Personnes { get; set; }
+
+    }
+}
---
+++ CoursEntityFrameWork/CoursLinqToEntities/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursLinqToEntities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContext data = new DataContext();
+            // ajouter un jeu de donnée
+            //data.Personnes.Add(new Personne { Nom = "toto", Prenom = "tata" });
+            //data.Personnes.Add(new Personne { Nom = "titi", Prenom = "minet" });
+            //data.SaveChanges();
+
+            //Linq to entites
+
+            //List<Personne> mPersonnes = (from p in data.Personnes where p.Prenom.Contains("m") select p).ToList();
+            // <=> lambda expression
+            List<Personne> mPersonnes = data.Personnes.Where(p => p.Prenom.Contains("m")).ToList();
+            foreach(Personne p in mPersonnes)
+            {
+                Console.WriteLine(p.Nom + " " + p.Prenom);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
